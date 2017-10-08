d504e1ba296aeb7d848ac59e3668f65620205fa7
Alistair Willis <alistairrwillis@gmail.com>
2017-10-08 21:15:48 +0900

    refactor code into separate GET requests

---
+++ README.md
@@ -0,0 +1,10 @@
+Support two routes 
+
+At /route?url= 
+- take in new routes to be minified 
+- check that they are valid urls (regex); 
+
+At / 
+- take in mini urls 
+- check that they are valid mini urls 
+- redirect 
--- app.js
+++ app.js
@@ -5,62 +5,78 @@ const fs = require('fs');
 const dataFile = path.join(__dirname + '/data.json');
 const app = express(); 
 
-let PORT = 3000; 
+let PORT = process.env.PORT || 3000; 
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
-})
+});
 
+// create a new mini URL
 app.get('/route?', (req, res) => {
-  console.log(req.hostname);
-  console.log(req.url);
   const url = req.query.url; 
+  const regex = /^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/;
+
+  if (!url.match(regex)) {
+    res.status(400).json({error: 'invalid url'});
+  } 
+  
+  else {
+    fs.readFile(dataFile, (err, data) => {
+      if (err) handleError(500);
+        
+      const dataObject = JSON.parse(data.toString('utf8'));
+      let { count, urls } = dataObject; 
+
+      let localport = process.env.NODE_ENV === undefined ? 3000 : undefined;
+  
+      const shortUrl = `http://${req.hostname}:${localport}/mini/${count++}`;
+      const newRecord = { 
+        originalUrl: url, 
+        shortUrl: shortUrl
+      };
+      urls.push(newRecord);  
+      dataObject.count = count;
+    
+      fs.writeFile(dataFile, JSON.stringify(dataObject), (err) => {
+        if (err) handleError(500);
+        res.status(200).json({shortUrl: shortUrl});
+        res.end();
+      });
+    });
+  }
+});
+
+// redirect an existing mini URL
+app.get('/mini/:count', (req, res) => {
+  const count = req.params.count; 
+  if (!count.match(/[0-9]+/g)) {
+    res.status(404).send({error: 'URL not found'});
+  }
   fs.readFile(dataFile, (err, data) => {
-    if (err) throw err; 
-
-    const dataObject = JSON.parse(data.toString('utf8'));
-    let { count, urls } = dataObject; 
-
-    if (isMiniUrl(url)) {
-      const record = getRecord(url, dataObject);
-      if (!record) {
-        res.status(200).json({error: 'URL does not exist'});
-      } else {
-        res.redirect(301, record.originalUrl);
-        // res.status(200).send(record);
-      }
-    } else {
-        let shortUrl = `mini-${count++}`;
-        const newRecord = { 
-          originalUrl: url, 
-          shortUrl: shortUrl
-        };
-        urls.push(newRecord);  
-        dataObject.count = count;
+    if (err) handleError(500);
       
-        fs.writeFile(dataFile, JSON.stringify(dataObject), (err) => {
-          if (err) throw err;
-          res.status(200).json({shortUrl: shortUrl});
-          res.end();
-        })
-      } 
+    const dataObject = JSON.parse(data.toString('utf8'));
+    const record = getRecord(count, dataObject);
+    if (!record) {
+      res.status(404).json({error: 'URL does not exist'});
+    } else {
+      res.redirect(301, record.originalUrl);
+    }
   });
-})
+});
+
+function getRecord(count, dataObject) {
+  const recordFound = dataObject.urls[count]; 
+  return recordFound || null;
+}
+
+function handleError(statusCode) {
+ switch (statusCode) {
+   case 500: 
+    res.status(500).send({error: 'The server could not handle your request'});
+ }
+}
 
 app.listen(PORT, () => {
   console.log(`Listening on Port: ${PORT}`);
 })
-
-
-function isMiniUrl(url) {
-  return url.match(/mini-[0-9]+/g);
-}
-
-function getRecord(miniUrl, dataObject) {
-  const index = miniUrl.slice(5);
-  const recordFound = dataObject.urls[index]; 
-
-  if (!recordFound) return null;
-  else if (recordFound.shortUrl = miniUrl) return recordFound;
-  else return null;
-}
